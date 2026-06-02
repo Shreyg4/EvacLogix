fe6fbd07d349e6e9a685583a4913b5a9725e776b
Yeetnade <shreyassundar0305@gmail.com>
2026-06-02 12:19:20 -0700

    New build and memory fix

--- Assets/Editor/EvacLogixWebGlBuilder.cs
+++ Assets/Editor/EvacLogixWebGlBuilder.cs
@@ -11,6 +11,13 @@ namespace EvacLogix.Editor
         [UnityEditor.MenuItem("EvacLogix/Build WebGL")]
         public static void Build()
         {
+            PlayerSettings.WebGL.memorySize = 512;
+#if UNITY_6000_0_OR_NEWER
+            PlayerSettings.WebGL.memoryGrowthMode = WebGLMemoryGrowthMode.Linear;
+            PlayerSettings.WebGL.initialMemorySize = 512;
+            PlayerSettings.WebGL.maximumMemorySize = 2048;
+#endif
+
             var outputPath = Path.GetFullPath("EvacLogixBuild");
             var scenes = EditorBuildSettings.scenes
                 .Where(scene => scene.enabled)
